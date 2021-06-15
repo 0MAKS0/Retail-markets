e7c8560217da776e69e61cb57131955137e77049
maks_dobrov03@inbox.ru <maks_dobrov03@inbox.ru>
2021-06-15 21:28:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿// OpenWeatherMap REST API example
+// DKY 2021
+
+using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ApiKey = "d864a56ba60c223cf96aae9c485e48f2";
+            //var City = "Kazan";
+            var url = $"https://apidata.mos.ru/v1/datasets/654/rows?api_key={ApiKey}";
+
+            var request = WebRequest.Create(url);
+
+            var response = request.GetResponse();
+            var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+            if (httpStatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine(httpStatusCode);
+                return;
+            }
+
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                Console.WriteLine(result);
+                var weatherForecast = JsonConvert.DeserializeObject<@class>(result);
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp1/class.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class @class
+    {
+        public class GeoData
+        {
+            public List<double> coordinates { get; set; }
+            public string type { get; set; }
+        }
+
+        public class Cells
+        {
+            public string Name { get; set; }
+            public int global_id { get; set; }
+            public string AdmArea { get; set; }
+            public string District { get; set; }
+            public string Address { get; set; }
+            public string ManagementCompany { get; set; }
+            public string LegalAddressOfManagementCompany { get; set; }
+            public string MarketType { get; set; }
+            public string ExtraInfo { get; set; }
+            public GeoData geoData { get; set; }
+        }
+
+        public class Root
+        {
+            public int global_id { get; set; }
+            public int Number { get; set; }
+            public Cells Cells { get; set; }
+        }
+
+
+
+    }
+}
